dcd67dbc7fbc1b2fbba75f95086b11f46bde3622
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 10:44:11 -0400

    Gas_Calculator_rev_1

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ var myMiles = prompt("Let's Determine how much gas is used per day to go to work
 var myMPG = prompt("Please enter the miles per gallon."); //user enters the miles per gallon their vehicle uses.
 var myGPD = myMiles / myMPG; // calculates the miles driven divided by the vehicle's miles per gallon.
 //console.log(myGPD); //prints out to the console.
-var result = "You use roughly " + myGPD + "Gallons of gas per day to work.";
+var result = "You use roughly " + myGPD + "Gallons of gas per day to drive to work.";
 alert(result);
