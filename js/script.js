82365a2a7371923278dc82dcc12dccdb621648c0
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 11:35:30 -0400

    GUC_rev_7

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ var resultGPD = "You use roughly " + myGPD + "Gallons of gas per day to drive to
 alert(resultGPD);
 var myDays = prompt("How many days do you work per week? Please enter the total days.") * 50; //user enters the weekly amount of days they work.
 var myGTY = myDays * myGPD; // calculates the total of yealy days multiplied by the vehicle's gallons per day.
-//console.log(myGPD); //prints out to the console.
+//console.log(myGTY); //prints out to the console.
 var resultGTY = "You use roughly " + myGTY + "Gallons of gas per year to drive to work."; //Javascript displays the totals
 alert(resultGTY);
+var myGPrice = prompt("How much is the cost of a gallon of gas at your local gas station? Enter the Price Per Gallon. $"); //user enters the price of gas per gallon.
+var myGETY = myGPrice * myGTY; // calculates the total of yearly cost of gas.
+//console.log(myGETY); //prints out to the console.
+var resultGETY = "You use roughly $" + myGTY + "Dollars of your hard earned cash per year to drive to work."; //Javascript displays the totals
+alert(resultGETY);
