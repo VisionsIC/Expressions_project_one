533e6342ad9863dd595a3774248376ac681d02b3
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 11:01:54 -0400

    GUC_rev_4

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ var myMiles = prompt("Use Google Maps to determine the distance in miles it is t
 var myMPG = prompt("Google search your vehicle manufactorer to find out your MPG. Please enter the miles per gallon."); //user enters the miles per gallon their vehicle uses.
 var myGPD = myMiles / myMPG; // calculates the miles driven divided by the vehicle's miles per gallon.
 //console.log(myGPD); //prints out to the console.
-var result = "You use roughly " + myGPD + "Gallons of gas per day to drive to work.";
-alert(result);
+var resultGPD = "You use roughly " + myGPD + "Gallons of gas per day to drive to work.";
+alert(resultGPD);
