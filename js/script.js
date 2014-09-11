6c27e0344bffec47643814f35dbe94019f8728bd
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 11:04:14 -0400

    GUC_rev_5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Prompt and Alert lesson.
+//Project One - Gas Usage Calculator.
 var myMiles = prompt("Use Google Maps to determine the distance in miles it is to your work. Please enter the miles.") * 2; //user enters the miles per day their vehicle travels to work.
 var myMPG = prompt("Google search your vehicle manufactorer to find out your MPG. Please enter the miles per gallon."); //user enters the miles per gallon their vehicle uses.
 var myGPD = myMiles / myMPG; // calculates the miles driven divided by the vehicle's miles per gallon.
