3eb003276635ace3691df828aa7e9e337422f2d9
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 10:57:58 -0400

    GUC_rev_3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //Prompt and Alert lesson.
-var myMiles = prompt("Let's Determine how much gas is used per day to go to work. Please enter the miles."); //user enters the miles per day their vehicle travels to work.
-var myMPG = prompt("Please enter the miles per gallon."); //user enters the miles per gallon their vehicle uses.
+var myMiles = prompt("Use Google Maps to determine the distance in miles it is to your work. Please enter the miles.") * 2; //user enters the miles per day their vehicle travels to work.
+var myMPG = prompt("Google search your vehicle manufactorer to find out your MPG. Please enter the miles per gallon."); //user enters the miles per gallon their vehicle uses.
 var myGPD = myMiles / myMPG; // calculates the miles driven divided by the vehicle's miles per gallon.
 //console.log(myGPD); //prints out to the console.
 var result = "You use roughly " + myGPD + "Gallons of gas per day to drive to work.";
