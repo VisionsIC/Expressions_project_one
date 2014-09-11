f3889149e4be1337d57511d4265b8f0e4c6b7ee5
Aaron Wilson <wilsonaarob@yahoo.com>
2014-09-11 11:21:52 -0400

    GUC_rev_6

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ var myGPD = myMiles / myMPG; // calculates the miles driven divided by the vehic
 //console.log(myGPD); //prints out to the console.
 var resultGPD = "You use roughly " + myGPD + "Gallons of gas per day to drive to work.";
 alert(resultGPD);
+var myDays = prompt("How many days do you work per week? Please enter the total days.") * 50; //user enters the weekly amount of days they work.
+var myGTY = myDays * myGPD; // calculates the total of yealy days multiplied by the vehicle's gallons per day.
+//console.log(myGPD); //prints out to the console.
+var resultGTY = "You use roughly " + myGTY + "Gallons of gas per year to drive to work."; //Javascript displays the totals
+alert(resultGTY);
